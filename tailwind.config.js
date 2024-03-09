4fc5281356dfbcbfd106daa3fc2ca25456c496de
Chun-gu <chungu.yi92@gmail.com>
2024-03-09 23:29:29 +0900

    미완

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./pages/**/*.{html,js}", "./components/**/*.{html,js}"],
+  content: [
+    "./index.html",
+    "./src/pages/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        blossom: "url(/blossom_bg_1.png)",
+      },
+    },
   },
   plugins: [],
 };
