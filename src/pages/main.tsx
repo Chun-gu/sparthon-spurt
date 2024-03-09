65793c46c2e5569a2232e2372e9fec79fb5192d0
Chun-gu <chungu.yi92@gmail.com>
2024-03-10 03:41:10 +0900

    style: prettier + eslint 적용

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,36 +1,34 @@
 export function MainPage() {
-  function onSubmitBucket() {}
+	function onSubmitBucket() {}
 
-  return (
-    <main className="min-h-screen bg-blossom bg-no-repeat bg-cover flex justify-center">
-      <section className="bg-white/20 flex flex-col items-center">
-        <h1 className="text-5xl text-red-500">Sparthon Bucket List</h1>
-        <h2>꿈은 꾸는 것이 아닌, 이루는 것이다.</h2>
+	return (
+		<main className="min-h-screen bg-blossom bg-no-repeat bg-cover flex justify-center">
+			<section className="bg-white/20 flex flex-col items-center">
+				<h1 className="text-5xl text-red-500">Sparthon Bucket List</h1>
+				<h2>꿈은 꾸는 것이 아닌, 이루는 것이다.</h2>
 
-        <audio src="/bgm.mp3"></audio>
+				<form onSubmit={onSubmitBucket}>
+					<input type="text" />
+					<button>
+						<img src="/add_icon.png" />
+						<span className="sr-only">제출</span>
+					</button>
+				</form>
 
-        <form onSubmit={onSubmitBucket}>
-          <input type="text" />
-          <button>
-            <img src="/add_icon.png" />
-            <span className="sr-only">제출</span>
-          </button>
-        </form>
-
-        <section>
-          <ul>
-            <li>
-              <div>
-                <span>웹 개발 시작하기</span>
-                <button>
-                  <img src="/delete_icon.png" alt="" />
-                  <span className="sr-only">삭제</span>
-                </button>
-              </div>
-            </li>
-          </ul>
-        </section>
-      </section>
-    </main>
-  );
+				<section>
+					<ul>
+						<li>
+							<div>
+								<span>웹 개발 시작하기</span>
+								<button>
+									<img src="/delete_icon.png" />
+									<span className="sr-only">삭제</span>
+								</button>
+							</div>
+						</li>
+					</ul>
+				</section>
+			</section>
+		</main>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/pages/**/*.{ts,tsx}",
-    "./src/components/**/*.{ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        blossom: "url(/blossom_bg_1.png)",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./index.html",
+		"./src/pages/**/*.{ts,tsx}",
+		"./src/components/**/*.{ts,tsx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				blossom: "url(/blossom_bg_1.png)",
+			},
+		},
+	},
+	plugins: [],
 };
